712762341d9cb6557eddc03fba78f22d46978fad
olamide05 <132193157+olamide05@users.noreply.github.com>
2023-07-03 18:43:02 +0100

    Add files via upload

---
+++ BMI CALCULATOR.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+HEIGHT=input("what is your hieght in meters:")
+WEIGHT=input("what is your weight in kg:")
+NEW_HEIGHT=int(HEIGHT)
+NEW_WEIGHT=int(WEIGHT)
+BMI=NEW_WEIGHT / (NEW_HEIGHT*NEW_HEIGHT)
+print(BMI)
+
+
+# In[ ]:
+
+
+
+
